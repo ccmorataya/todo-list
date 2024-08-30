f4ebe439bb2fda829c9a50c177982c925ad55a8e
Cristian Morataya <cris.morataya@gmail.com>
2024-08-30 13:11:15 -0600

    Update modal component to include form validation and reactive form module

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,14 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'todolist-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './modal.component.html',
 })
 export class ModalComponent {
   @Output() close = new EventEmitter<void>();
   showModal = false;
+  modalForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+  });
 
   toggleModal() {
     this.close.emit();
